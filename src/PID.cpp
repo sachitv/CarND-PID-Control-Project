5d0c2b2ab4a9cc6a3b0e70a32c64326ac4d9c410
Sachit Vithaldas <macintoshsac@gmail.com>
2017-06-04 12:04:31 -0700

    GitIgnore + First Working Edition

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,6 @@
 #include "PID.h"
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,16 +8,210 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+static int const MIN_STEPS = 200;
+
+PID::PID()
+		: p_error( 0.f )
+		  , i_error( 0.f )
+		  , d_error( 0.f )
+		  , Kp( 0.f )
+		  , Ki( 0.f )
+		  , Kd( 0.f )
+		  , lastCTE( 0.f )
+		  , tolerance( 0.2f )
+		  , lastdpD( 1.f )
+		  , lastdpI( 1.f )
+		  , lastdpP( 1.f )
+		  , twiddleStage( TwiddleStage::ZeroRuns )
+		  , twiddlerParam( TwiddlerParam::P )
+		  , bestErr( numeric_limits<double>::max())
+		  , totalError( 0.f )
+		  , numDatas( 0 ) {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init( double inKp, double inKi, double inKd )
+{
+	Kp = inKp;
+	Kd = inKd;
+	Ki = inKi;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError( double cte )
+{
+	p_error = cte;
+	d_error = cte - lastCTE;
+	i_error += cte;
+
+	lastCTE = cte;
+
+	numDatas += 1;
+
+	if ( numDatas > MIN_STEPS )
+	{
+		totalError = (totalError * (numDatas - 1) + (cte * cte)) / numDatas;
+	}
 }
 
-double PID::TotalError() {
+double PID::GetSteering() const
+{
+	double amount = -(Kp * p_error) - (Kd * d_error) - (Ki * i_error);
+	amount = max( -1.0, amount );
+	amount = min( 1.0, amount );
+
+	return amount;
+}
+
+void PID::Twiddle()
+{
+	double const error = totalError;
+	switch ( twiddleStage )
+	{
+		case TwiddleStage::ZeroRuns:
+		{
+			switch ( twiddlerParam )
+			{
+				case TwiddlerParam::P:
+				{
+					double const sum = lastdpP + lastdpI + lastdpD;
+					if ( sum > tolerance )
+					{
+						Kp += lastdpP;
+
+						//Go to the next stage on the next run
+						twiddleStage = TwiddleStage::FirstRun;
+					}
+				}
+					break;
+				case TwiddlerParam::I:
+				{
+					Ki += lastdpI;
+
+					//Go to the next stage on the next run
+					twiddleStage = TwiddleStage::FirstRun;
+				}
+					break;
+				case TwiddlerParam::D:
+				{
+					Kd += lastdpD;
+
+					//Go to the next stage on the next run
+					twiddleStage = TwiddleStage::FirstRun;
+				}
+					break;
+			}
+		}
+			break;
+		case TwiddleStage::FirstRun:
+		{
+			if ( error < bestErr )
+			{
+				bestErr = error;
+				twiddleStage = TwiddleStage::ZeroRuns;
+				switch ( twiddlerParam )
+				{
+					case TwiddlerParam::P:
+					{
+						lastdpP *= 1.1;
+						twiddlerParam = TwiddlerParam::I;
+					}
+						break;
+					case TwiddlerParam::I:
+					{
+						lastdpI *= 1.1;
+						twiddlerParam = TwiddlerParam::D;
+					}
+						break;
+					case TwiddlerParam::D:
+					{
+						lastdpD *= 1.1;
+						twiddlerParam = TwiddlerParam::P;
+					}
+						break;
+				}
+			}
+			else
+			{
+				//Next stage is the second run
+				twiddleStage = TwiddleStage::SecondRun;
+				switch ( twiddlerParam )
+				{
+					case TwiddlerParam::P:
+					{
+						Kp -= 2 * lastdpP;
+					}
+						break;
+					case TwiddlerParam::I:
+					{
+						Ki -= 2 * lastdpI;
+					}
+						break;
+					case TwiddlerParam::D:
+					{
+						Kd -= 2 * lastdpD;
+					}
+						break;
+				}
+			}
+		}
+			break;
+		case TwiddleStage::SecondRun:
+		{
+			twiddleStage = TwiddleStage::ZeroRuns;
+
+			if ( error < bestErr )
+			{
+				bestErr = error;
+				switch ( twiddlerParam )
+				{
+					case TwiddlerParam::P:
+					{
+						lastdpP *= 1.1;
+						twiddlerParam = TwiddlerParam::I;
+					}
+						break;
+					case TwiddlerParam::I:
+					{
+						lastdpI *= 1.1;
+						twiddlerParam = TwiddlerParam::D;
+					}
+						break;
+					case TwiddlerParam::D:
+					{
+						lastdpD *= 1.1;
+						twiddlerParam = TwiddlerParam::P;
+					}
+						break;
+				}
+			}
+			else
+			{
+				switch ( twiddlerParam )
+				{
+					case TwiddlerParam::P:
+					{
+						Kp += lastdpP;
+						lastdpP *= 0.9f;
+						twiddlerParam = TwiddlerParam::I;
+					}
+						break;
+					case TwiddlerParam::I:
+					{
+						Ki += lastdpI;
+						lastdpI *= 0.9f;
+						twiddlerParam = TwiddlerParam::D;
+					}
+						break;
+					case TwiddlerParam::D:
+					{
+						Kd += lastdpD;
+						lastdpD *= 0.9f;
+						twiddlerParam = TwiddlerParam::P;
+					}
+						break;
+				}
+			}
+		}
+	}
 }
 
--- src/PID.h
+++ src/PID.h
@@ -1,46 +1,82 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
+class PID
+{
+	enum class TwiddleStage
+	{
+		ZeroRuns,
+		FirstRun,
+		SecondRun
+	};
+	enum class TwiddlerParam
+	{
+		P,
+		I,
+		D
+	};
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+	/*
+	* Errors
+	*/
+	double p_error;
+	double i_error;
+	double d_error;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+	/*
+	* Coefficients
+	*/
+	double Kp;
+	double Ki;
+	double Kd;
 
-  /*
-  * Constructor
-  */
-  PID();
+	/*
+	 * Other Stuff
+	 */
+	double lastCTE;
+	double const tolerance;
+	double lastdpP;
+	double lastdpI;
+	double lastdpD;
+	double bestErr;
+	double totalError;
+	int numDatas;
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+	TwiddleStage twiddleStage;
+	TwiddlerParam twiddlerParam;
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
 
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+
+	/*
+	* Constructor
+	*/
+	PID();
+
+	/*
+	* Destructor.
+	*/
+	virtual ~PID();
+
+	/*
+	* Initialize PID.
+	*/
+	void Init( double inKp, double inKi, double inKd );
+
+	/*
+	* Update the PID error variables given cross track error.
+	*/
+	void UpdateError( double cte );
+
+	/*
+	* Calculate the total PID error.
+	*/
+	double GetSteering() const;
+
+	/*
+	 * Recalculate Parameters
+	 */
+	void Twiddle();
 };
 
 #endif /* PID_H */
